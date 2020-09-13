05498f17d415545a0392baed1d8501a8e7d011cb
PHRUEK APIROMRAT <63010650@KMITL.AC.TH>
2020-09-13 18:06:29 +0700

    divide

--- assignment14/Source.cpp
+++ assignment14/Source.cpp
@@ -1,12 +1,15 @@
 #include<stdio.h>
 int main() {
-	int num, i;
+	int num,num2, i;
 	printf("enter the number : ");
 	scanf_s("%d", &num);
+	printf("enter the number2 : ");
+	scanf_s("%d", &num2);
 	i = 0;
 	do
 	{
-		if ((i % 4) == 0) printf("%d ", i);
+		if ((i % num2) == 0) 
+			printf("%d ", i);
 		i++;
 	}
 	while (i <= num);
